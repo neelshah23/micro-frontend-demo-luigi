9e173b091b0048aa7b4715a46992307d8e2e86fd
neelshah <neel.shah@iauro.com>
2019-09-13 14:36:13 +0530

    Neel: update custom component and new NAD application

--- micro-frontend-demo2/src/app/app.component.ts
+++ micro-frontend-demo2/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as luigiClient from '@kyma-project/luigi-client';
-import {addContextUpdateListener, getClientPermissions, uxManager} from '@kyma-project/luigi-client/luigi-client';
+import {addContextUpdateListener} from '@kyma-project/luigi-client/luigi-client';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,14 +9,19 @@ import {addContextUpdateListener, getClientPermissions, uxManager} from '@kyma-p
 export class AppComponent implements OnInit {
   title = 'micro-frontend-demo2';
   cudListener: any;
+  clickCount = 0;
 
   ngOnInit(): void {
-    this.cudListener = addContextUpdateListener(updatedContext => {
-      console.log(updatedContext);
-    });
+    // this.cudListener = addContextUpdateListener(updatedContext => {
+    //   console.log(updatedContext);
+    // });
   }
 
   goToReactApp() {
     luigiClient.linkManager().navigate('/home/three', null, false);
+  }
+  buttonClick(message) {
+    ++this.clickCount;
+    alert(message);
   }
 }
